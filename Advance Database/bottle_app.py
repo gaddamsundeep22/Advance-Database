8a97b5f530eecfba326a066117122ea28cde2621
gaddamsundeep22 <gaddamsundeep22@gmail.com>
2022-10-25 23:48:40 +0000

    done

--- Advance Database/bottle_app.py
+++ Advance Database/bottle_app.py
@@ -1,10 +1,40 @@
+from bottle import default_app, route, get, post, template, request, redirect
 
-# A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app, route
+import database
 
 @route('/')
-def hello_world():
-    return 'Hello from Sundeep Gaddam '
+def get_index():
+    redirect('/list')
+
+@route('/list')
+def get_list():
+    items = database.get_items()
+    return template("shopping_list.tpl", name="Dr. DeLozier", shopping_list=items)
+
+@post('/add')
+def post_add():
+    description = request.forms.get("description")
+    database.add_item(description)
+    redirect('/list')
+
+@route("/delete/<id>")
+def get_delete(id):
+    database.delete_item(id)
+    redirect('/list')
+
+@get("/edit/<id>")
+def get_edit(id):
+    items = database.get_items(id)
+    if len(items) != 1:
+        redirect('/list')
+    item_id, description = items[0]['id'], items[0]['desc']
+    assert item_id == int(id)
+    return template("edit_item.tpl", id=id, description=description)
+
+@post("/edit/<id>")
+def post_edit(id):
+    description = request.forms.get("description")
+    database.update_item(id, description)
+    redirect('/list')
 
 application = default_app()
-
