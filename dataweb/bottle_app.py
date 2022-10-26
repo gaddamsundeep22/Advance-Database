8f24e686021bd01e4f83791d843e1f6c6f56d0b4
gaddamsundeep22 <gaddamsundeep22@gmail.com>
2022-10-26 23:49:32 +0000

    Done

--- dataweb/bottle_app.py
+++ dataweb/bottle_app.py
@@ -1,63 +1,40 @@
+from bottle import default_app, route, get, post, template, request, redirect
 
-# A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app, route, template, get, post, request, redirect
-import sqlite3
+import database
 
-connection = sqlite3.connect("shopping_list.db")
-
-@route('/hi')
-def hi_world():
-    return 'Hi from Sundeep!'
-
-@route('/hello')
-def hello_world():
-    return 'Hello from Sundeep!'
+@route('/')
+def get_index():
+    redirect('/list')
 
 @route('/list')
 def get_list():
-    cursor = connection.cursor()
-    rows = cursor.execute("select id, description from list")
-    rows = list(rows)
-    rows = [ {'id':row[0] ,'desc':row[1]} for row in rows ]
-    return template("shopping_list.tpl", name = "Sundeep", shopping_list=rows)
-
-@get('/add')
-def get_add():
-    return template("add_item.tpl")
+    items = database.get_items()
+    return template("shopping_list.tpl", name="Sundeep", shopping_list=items)
 
 @post('/add')
 def post_add():
     description = request.forms.get("description")
-    cursor = connection.cursor()
-    cursor.execute(f"insert into list (description) values ('{description}')")
-    connection.commit()
+    database.add_item(description)
     redirect('/list')
 
 @route("/delete/<id>")
 def get_delete(id):
-    cursor = connection.cursor()
-    cursor.execute(f"delete from list where id={id} ")
-    connection.commit()
+    database.delete_item(id)
     redirect('/list')
 
 @get("/edit/<id>")
 def get_edit(id):
-    cursor = connection.cursor()
-    items = cursor.execute(f"select description from list where id={id}")
-    items = list(items)
+    items = database.get_items(id)
     if len(items) != 1:
         redirect('/list')
-    description = items[0][0]
+    item_id, description = items[0]['id'], items[0]['desc']
+    assert item_id == int(id)
     return template("edit_item.tpl", id=id, description=description)
 
 @post("/edit/<id>")
 def post_edit(id):
     description = request.forms.get("description")
-    cursor = connection.cursor()
-    cursor.execute(f"update list set description='{description}' where id={id}")
-    connection.commit()
+    database.update_item(id, description)
     redirect('/list')
 
-
 application = default_app()
-
--- dataweb/views/shopping_list.tpl
+++ dataweb/views/shopping_list.tpl
@@ -1,18 +1,20 @@
 <html>
 <body>
-<h3> Shopping list </h3>
+<h2>Shopping List</h2>
+<hr/>
 <table>
 % for item in shopping_list:
-    <tr>
-
-       <td>{{str(item['desc'])}}</td>
-
-       <td><a href="/edit/{{str(item['id'])}}">edit</a></td>
-       <td><a href="/delete/{{str(item['id'])}}">delete</a></td>
-    </tr>
+  <tr>
+    <td>{{str(item['desc'])}}</td>
+    <td><a href="/edit/{{str(item['id'])}}">edit</a></td>
+    <td><a href="/delete/{{str(item['id'])}}">delete</a></td>
+  </tr>
 % end
 </table>
-<td><a href ="/add">New</a></td>
 <hr/>
+<form action="/add" method="post">
+  <p>Add new item: <input name="description"/></p>
+  <p><button type="submit">Submit</button>
+</form>
 </body>
 </html>
